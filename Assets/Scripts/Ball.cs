4fbd8392c624ca49675924d0c6e221058c00e4fe
Marshall Eldridge <l2marshall@gmail.com>
2021-09-08 17:24:11 -0600

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+// config params
+    [SerializeField] private Paddle paddle1;
+    [SerializeField] private float xPush = 0f;
+    [SerializeField] private float yPush = 15f;
+
+
+    // state
+    private Vector2 paddleToBallVector;
+    private bool hasStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
