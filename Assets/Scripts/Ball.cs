7a48d1003ee234a8ff76f30051d548bcc04c623f
Marshall Eldridge <l2marshall@gmail.com>
2021-09-09 11:31:27 -0600

    Game Working

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +9,21 @@ public class Ball : MonoBehaviour
     [SerializeField] private Paddle paddle1;
     [SerializeField] private float xPush = 0f;
     [SerializeField] private float yPush = 15f;
+    [SerializeField] private AudioClip[] ballSounds;
 
 
     // state
     private Vector2 paddleToBallVector;
     private bool hasStarted = false;
+    
+    //Cached component references
+    private AudioSource myAudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -44,5 +50,14 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
     }
 }
