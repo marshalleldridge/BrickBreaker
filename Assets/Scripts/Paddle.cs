8ed3d79b73a4e61ed9260fda7bc33de0622284c7
Marshall Eldridge <l2marshall@gmail.com>
2021-09-13 11:42:46 -0600

    Game Complete

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -7,19 +7,36 @@ public class Paddle : MonoBehaviour
     [SerializeField] float screenWidthInUnits = 16f;
     [SerializeField] float minX = 1f;
     [SerializeField] private float maxX = 15f;
+    
+    //chched references
+    private GameSession theGameSession;
+    private Ball theBall;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        theGameSession = FindObjectOfType<GameSession>();
+        theBall = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
         transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if (theGameSession.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
     }
 }
